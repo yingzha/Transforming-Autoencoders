5aa91c46811fb543c426011112ad2d783e902c15
Ying Zhang <ying.zhlisa@gmail.com>
2015-04-06 22:04:12 -0400

    add cost function

---
+++ cost.py
@@ -0,0 +1,27 @@
+__author__ = 'Ying Zhang'
+__copyright__ = 'Copyright 2015, University of Montreal'
+
+import numpy as np
+import theano
+import theano.tensor as T
+
+class Cost(object):
+    def __init__(self, model, input, extra_input):
+        self.model = model
+        self.y_pred = self.model.fprop(input, extra_input)
+
+    def L1_regu(self):
+        return sum([(abs(param)).sum() for param in self.model.params])
+
+    def L2_regu(self):
+        return sum([(param **2).sum() for param in self.model.params])
+
+    def mse(self, y):
+        loss = T.mean(((self.y_pred - y) ** 2).sum())
+        return loss
+
+    def cross_entropy(self, y):
+        return T.nnet.categorical_crossentropy(self.y_pred, y)
+
+    def get_ouput(self):
+        return self.y_pred
